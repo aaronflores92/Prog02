9568fc6f05f8f42120c850f4bc7e032916511bba
aaronflores92 <31676547+aaronflores92@users.noreply.github.com>
2017-10-26 20:49:47 -0700

    list fully implemented
    the list now maintains ordering by name and by weight using two sets of head and tail nodes. it also prints the list ordered by name or by tail.

--- main.cpp
+++ main.cpp
@@ -33,124 +33,131 @@ class Node
 class DoubleList
 {
 	public:
-		DoubleList() : head(0), tail(0), lsize(0) { } // 33, tail(0) { }
+		DoubleList() : head_by_weight(0), tail_by_weight(0), head_by_name(0), tail_by_name(0), lsize(0) { } // 33, tail_by_weight(0) { }
 		//~ DoubleList(); 
-		void add_by_weight(int & val, std::string name);
-		void add_by_name(int & val, std::string name);
-		bool empty() {return head == 0; }
-		void move_forward(int n);
+		void add(int & val, std::string name);
+		//void add_by_name(int & val, std::string name);
+		bool weight_empty() {return head_by_weight == 0; }
+		bool name_empty() {return head_by_name == 0; }
+		void print_by_weight(int n);
+		void print_by_name(int n);
 		int size() {return lsize; }
 		
 	//private:
-		Node * head;
-		Node * tail;
+		Node * head_by_weight;
+		Node * tail_by_weight;
+		Node * head_by_name;
+		Node * tail_by_name;
 		int lsize;
 };
 
-void DoubleList::add_by_weight(int & val, std::string name)
+void DoubleList::add(int & val, std::string name)
 {	
-	Node * N = new Node(val, name); 								//creates new node that will be added
-	if(empty())		// if the list is empty, insert new node and make it head & tail
+	Node * N_by_weight = new Node(val, name); 								//creates new node that will be added
+	if(weight_empty())		// if the list is empty, insert new node and make it head_by_weight & tail_by_weight
 	{
-		head = tail = N;
-		lsize++;
+		head_by_weight = tail_by_weight = N_by_weight;
 	}
 	
-	else if(head->weight >= N->weight)		//if new node weight is smaller than head weight, insert new node before head and set as new head
+	else if(head_by_weight->weight >= N_by_weight->weight)		//if new node weight is smaller than head weight, insert new node before head_by_weight and set as new head_by_weight
 	{
-		head->prev_link = N;
-		N->next_link = head;
-		head = N;
-		lsize++;
+		head_by_weight->prev_link = N_by_weight;
+		N_by_weight->next_link = head_by_weight;
+		head_by_weight = N_by_weight;
 	}
 	
-	else if(tail->weight <= N->weight)		//if new node weight is larger than tail weight, insert new node after tail and set as new tail
+	else if(tail_by_weight->weight <= N_by_weight->weight)		//if new node weight is larger than tail weight, insert new node after tail and set as new tail
 	{
-		tail->next_link = N;
-		N->prev_link = tail;
-		tail = N;
-		lsize++;
+		tail_by_weight->next_link = N_by_weight;
+		N_by_weight->prev_link = tail_by_weight;
+		tail_by_weight = N_by_weight;
 	}
 	
-	else if(head->weight < N->weight and tail->weight > N->weight)		//if new node weight is between head and tail weight, insert in appropriate spot between head and tail
+	else if(head_by_weight->weight < N_by_weight->weight and tail_by_weight->weight > N_by_weight->weight)		//if new node weight is between head and tail weight, insert in appropriate spot between head and tail
 	{
 		int n = size(), i = 0;
 		Node * current = new Node();
-		current = head;
+		current = head_by_weight;
 		while(i < n)
 		{
-			if(N->weight >= current->weight && N->weight <= current->next_link->weight)
+			if(N_by_weight->weight >= current->weight && N_by_weight->weight <= current->next_link->weight)
 			{
-				N->next_link = current->next_link;
-				current->next_link->prev_link = N;
-				current->next_link = N;
-				N->prev_link = current;
+				N_by_weight->next_link = current->next_link;
+				current->next_link->prev_link = N_by_weight;
+				current->next_link = N_by_weight;
+				N_by_weight->prev_link = current;
 				break;
 			}
 			current = current->next_link;
 			i++;
 		}
-		lsize++;
 	}
 	
 	else std::cout << "better luck pal" << endl;
 	
-}
-
-void DoubleList::add_by_name(int & val, std::string name)
-{	
-	Node * N = new Node(val, name); 								//creates new node that will be added
-	if(empty())		// if the list is empty, insert new node and make it head & tail
+	
+	
+	Node * N_by_name = new Node(val, name); 								//creates new node that will be added
+	if(name_empty())		// if the list is empty, insert new node and make it head_by_weight & tail_by_weight
 	{
-		head = tail = N;
-		lsize++;
+		head_by_name = tail_by_name = N_by_name;
 	}
 	
-	else if(head->name >= N->name)		//if new node weight is smaller than head weight, insert new node before head and set as new head
+	else if(head_by_name->name >= N_by_name->name)		//if new node weight is smaller than head_by_weight weight, insert new node before head_by_weight and set as new head_by_weight
 	{
-		head->prev_link = N;
-		N->next_link = head;
-		head = N;
-		lsize++;
+		head_by_name->prev_link = N_by_name;
+		N_by_name->next_link = head_by_name;
+		head_by_name = N_by_name;
 	}
 	
-	else if(tail->name <= N->name)		//if new node weight is larger than tail weight, insert new node after tail and set as new tail
+	else if(tail_by_name->name <= N_by_name->name)		//if new node weight is larger than tail_by_weight weight, insert new node after tail_by_weight and set as new tail_by_weight
 	{
-		tail->next_link = N;
-		N->prev_link = tail;
-		tail = N;
-		lsize++;
+		tail_by_name->next_link = N_by_name;
+		N_by_name->prev_link = tail_by_name;
+		tail_by_name = N_by_name;
 	}
 	
-	else if(head->name < N->name and tail->name > N->name)		//if new node weight is between head and tail weight, insert in appropriate spot between head and tail
+	else if(head_by_name->name < N_by_name->name and tail_by_name->name > N_by_name->name)		//if new node weight is between head_by_weight and tail_by_weight weight, insert in appropriate spot between head_by_weight and tail_by_weight
 	{
 		int n = size(), i = 0;
 		Node * current = new Node();
-		current = head;
+		current = head_by_name;
 		while(i < n)
 		{
-			if(N->name >= current->name && N->name <= current->next_link->name)
+			if(N_by_name->name >= current->name && N_by_name->name <= current->next_link->name)
 			{
-				N->next_link = current->next_link;
-				current->next_link->prev_link = N;
-				current->next_link = N;
-				N->prev_link = current;
+				N_by_name->next_link = current->next_link;
+				current->next_link->prev_link = N_by_name;
+				current->next_link = N_by_name;
+				N_by_name->prev_link = current;
 				break;
 			}
 			current = current->next_link;
 			i++;
 		}
-		lsize++;
 	}
 	
 	else std::cout << "better luck pal" << endl;
-	
+	lsize++;
 }
 
-void DoubleList::move_forward(int n)
+void DoubleList::print_by_weight(int n)
 {
 	Node * current = new Node();
-	current = head;
+	current = head_by_weight;
+	int i = 0;
+	while(i < n)
+	{
+		std::cout << "Name: " << current->name << " Weight: " << current->weight << endl;
+		current = current->next_link;
+		i++;
+	}
+}
+
+void DoubleList::print_by_name(int n)
+{
+	Node * current = new Node();
+	current = head_by_name;
 	int i = 0;
 	while(i < n)
 	{
@@ -162,30 +169,26 @@ void DoubleList::move_forward(int n)
 
 int main()
 {
-	std:: cout << "Hello World!" << endl;
-	DoubleList by_weight, by_name;
-	
+	DoubleList test;
 	std::cout << "Adding to list..." << endl;
 	
 	int x = 3;
-	by_weight.add_by_weight(x, "Aaron");
-	by_name.add_by_name(x, "Aaron");
+	test.add(x, "Aaron");
 	
 	x = 5;
-	by_weight.add_by_weight(x, "Alex");
-	by_name.add_by_name(x, "Alex");
+	test.add(x, "Alex");
 	
 	x = 1;
-	by_weight.add_by_weight(x, "Daniela");
-	by_name.add_by_name(x, "Daniela");
+	test.add(x, "Daniela");
 	
 	x = 2;
-	by_weight.add_by_weight(x, "Delia");
-	by_name.add_by_name(x, "Delia");
+	test.add(x, "Delia");
 	
 	std::cout << "Displaying list ordered by weight:" << endl;
-	by_weight.move_forward(by_weight.size());
+	test.print_by_weight(test.size());
+	
 	std::cout << "Displaying list ordered by name:" << endl;
-	by_name.move_forward(by_name.size());
+	test.print_by_name(test.size());
+	
 	return 0;
 }
